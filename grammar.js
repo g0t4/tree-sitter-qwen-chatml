d06e10246a5217030d4f8ad86c5d386d598c58a5
Wes Higbee <wes.mcclure@gmail.com>
2026-06-06 12:14:10 -0500

    stab at basics of grammar

--- grammar.js
+++ grammar.js
@@ -13,8 +13,34 @@ import * as constants from "./constants.js";
 export default grammar({
   name: "qwen_chatml",
 
+  inline: $ => [
+    $.message_content,
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($.message),
+    // TODO when doing FIM you'll wanna switch to a sequence of choices for the top-level of source_file
+    // source_file: $ => seq(
+    //   repeat($.message),
+    //   fim_message => (fim_prefix, fim_suffix, fim_middle),
+    // ),
+
+    message: $ => seq($.im_start, $.message_content, $.im_end),
+
+    // message_content: $ => repeat($.any), // TODO constrain this at all?
+    message_content: $ => prec(-9, field("contents", $.text)),
+
+    text: $ => repeat1(choice(
+      /[^<]+/, // be greedy with any other char (not <)
+      /</ // force decision on single < which means it is allowed too just only one char at a time
+    )),
+
+
+    im_start: $ => token(constants.IM_START),
+    im_end: $ => token(constants.IM_END),
+    // final_token: $ => choice($.im_end, $.return_token, $.call_token) // TODO more than one stop/end token I care about for my parser?
+
+    think_open: $ => token(constants.THINK_OPEN),
+    think_close: $ => token(constants.THINK_CLOSE),
   }
 });
