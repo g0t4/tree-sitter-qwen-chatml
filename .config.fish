06737a27a3fbbf87f38eb7b6f30835508f654a5b
Wes Higbee <wes.mcclure@gmail.com>
2026-06-06 11:15:10 -0500

    use new auto run_ abbrs helper function in dotfiles

--- .config.fish
+++ .config.fish
@@ -1,6 +1,3 @@
 nvm use 22 # use v22 of node whenever I enter this repo
 
-abbr run_clean "npm run clean"
-abbr run_install "npm run install"
-abbr run_build "npm run build"
-abbr run_test "npm run test_corpus"
+__local_config__pacakge_json_scripts_to_run_abbrs
