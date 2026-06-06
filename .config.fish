6bdb1ecb3cc86798b7a3729770755242fd073df6
Wes Higbee <wes.mcclure@gmail.com>
2026-06-06 10:59:10 -0500

    config fish to auto switch to nvm 22 and setup some run abbrs (run_ is a common convention I am establishing across repos)

---
+++ .config.fish
@@ -0,0 +1,6 @@
+nvm use 22 # use v22 of node whenever I enter this repo
+
+abbr run_clean "npm run clean"
+abbr run_install "npm run install"
+abbr run_build "npm run build"
+abbr run_test "npm run test_corpus"
