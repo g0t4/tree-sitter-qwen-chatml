f5ff5955a0103886a5aee98f4dd2f80b2e90d2cf
Wes Higbee <wes.mcclure@gmail.com>
2026-06-07 06:27:30 -0500

    queries for highlights!!

--- .config.fish
+++ .config.fish
@@ -1,3 +1,6 @@
 nvm use 22 # use v22 of node whenever I enter this repo
 
 __local_config__pacakge_json_scripts_to_run_abbrs
+
+# representative examples for testing highlights/colors
+abbr highlight_fim "tree-sitter highlight test/corpus/fim_repo_level/repo_level.qwen_chatml"
