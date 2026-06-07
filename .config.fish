0bb969b560835695d1c7f300bad6d4540fc5e250
Wes Higbee <wes.mcclure@gmail.com>
2026-06-07 07:51:27 -0500

    abbrs

--- .config.fish
+++ .config.fish
@@ -4,3 +4,5 @@ __local_config__pacakge_json_scripts_to_run_abbrs
 
 # representative examples for testing highlights/colors
 abbr highlight_fim "tree-sitter highlight test/corpus/fim_repo_level/repo_level.qwen_chatml"
+abbr highlight_chat "tree-sitter highlight test/corpus/messages/multi.qwen_chatml"
+# abbr highlight_tool "tree-sitter highlight test/corpus/tools/tools1/raw_full.test" # TODO extract chatml only with script
